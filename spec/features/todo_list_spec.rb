bbcdbe9059d4a6aedab9e6a725536f3e63ca2da2
Aaron Glasenapp <glassbead@gmail.com>
2014-08-22 22:40:24 -0600

    updated test suite

--- spec/features/todo_list_spec.rb
+++ spec/features/todo_list_spec.rb
@@ -3,20 +3,24 @@ require 'spec_helper'
 
 describe 'todo list' do
   scenario 'can add a todo item' do
-    visit todos_path
+    @user = FactoryGirl.create(:user)
 
-    click_link 'New Tudu'
+    visit '/'
+    click_link 'Login'
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password
+    # click_button 'Log in'
 
-    fill_in 'Name', with: 'get me done!'
-    click_button 'Add'
-
-    expect(page).to have_text('get me done!')
+    # fill_in 'Name', with: 'get me done!'
+    # click_button 'Add'
+    #
+    # expect(page).to have_text('get me done!')
   end
 
   scenario 'can mark an item as done' do
     todo = FactoryGirl.create(:todo)
 
-    visit todos_path
+
 
 
   end
