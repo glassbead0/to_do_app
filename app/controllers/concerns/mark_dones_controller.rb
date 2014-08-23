b1e4b2d2b81c982ffc2bed187f43ecb51d53874d
Aaron Glasenapp <glassbead@gmail.com>
2014-08-23 15:27:09 -0600

    mark all as done function added for admins

--- app/controllers/concerns/mark_dones_controller.rb
+++ app/controllers/concerns/mark_dones_controller.rb
@@ -1,5 +1,5 @@
 class MarkDonesController < ApplicationController
-  def update
+  def update # mark one as done
     @user = current_user
     @todo = @user.todos.find(params[:id])
     respond_to do |format|
@@ -10,6 +10,23 @@ class MarkDonesController < ApplicationController
         format.html { render :edit }
         format.json { render json: @todo.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  def index # mark all as done
+    @user = current_user
+    @todos = @user.todos
+    respond_to do |format|
+
+      @todos.each do |todo|
+        todo.update_attribute(:done, true)
+      end
+        format.html { redirect_to todos_path }
+        format.json { render :show, status: :ok, location: @todo }
+      # else
+      #   format.html { render :edit }
+      #   format.json { render json: @todo.errors, status: :unprocessable_entity }
+      # end
     end
   end
 end
